ed7ce4575e8b9b1f92ff2d74c762bc2d3efb1fb0
xavier sàbat i badell <xsabat@uoc.edu>
2024-04-24 08:56:00 +0200

    first change

--- HelloWorld2019/Program.cs
+++ HelloWorld2019/Program.cs
@@ -9,7 +9,7 @@ namespace HelloWorld2019
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World 2024!");
         }
     }
 }
